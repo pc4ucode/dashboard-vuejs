251cbaf6db79f84de1ea9eac47f618283258a802
Euler Amorim <euler.amorim@hotmail.coomo>
2021-02-23 14:12:43 -0300

    aula 3 - configuração vue-router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+import { Vue } from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+Vue.use(VueRouter)
+
+const routes = [
+    {path: '/login', component: loginComponent},
+    {path: '/home', component: HomeComponent}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+Vue(App, router).mount('#app')
+
+// new Vue({
+//     router,
+//     render: h => h(App),
+// }).$mount('#app')
