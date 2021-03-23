c6867fb518ee429bcc5873719cd0d99a5756a754
pc4ucode <euler.amorim@hotmail.com>
2021-03-22 22:47:28 -0300

    aula 11 -criando o topo da sidebar

--- frontend/src/pages/Dashboard/components/SideBarComponent.vue
+++ frontend/src/pages/Dashboard/components/SideBarComponent.vue
@@ -6,13 +6,19 @@
             </div>
 
             <div class="info">
-                <p>Euler Amorim</p>
-                <span>online</span>
+                <p class="name">Euler Amorim</p>
+                <p class="status">online</p>
             </div>
         </div>
 
         <div class="menu">
-
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
     </div>
 </template>
@@ -30,8 +36,12 @@ export default {
         width: 100%;
         display: flex;
         justify-content: center;
+        align-items: center;
+        padding: 25px 5px;
+        margin-top: 60px;
 
         .avatar {
+            margin-right: 10px;
             
             span {
                 width: 50px;
@@ -41,10 +51,24 @@ export default {
                 display: flex;
                 justify-content: center;
                 align-items: center;
+                font-size: 25px;
+                color: #666;
+            }
+        }
 
-                i {
-                    font-size: 12px;
-                }
+        .info {
+            
+            p {
+                margin: 0;
+                color: #fff;
+            }
+
+            .name {
+                font-size: 14px;
+            }
+
+            .status {                
+                font-size: 10px;
             }
         }
     }
